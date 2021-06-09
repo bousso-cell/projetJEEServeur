f84f83c367b72764d6ba3559eb95aa6feda2ca5d
Aminf17 <amadoubousso50@gmail.com>
2021-06-09 21:34:23 +0000

    Bousso's first commit

---
+++ src/main/java/com/example/projetjee/Busso/TypeMouvement.java
@@ -0,0 +1,56 @@
+package com.example.projetjee.Busso;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class TypeMouvement {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String codeTypeMvt;
+    private String libelleTypeMvt;
+    private long prixTypeMvt;
+
+    public TypeMouvement(String libelleTypeMvt, long prixTypeMvt) {
+        this.libelleTypeMvt = libelleTypeMvt;
+        this.prixTypeMvt = prixTypeMvt;
+    }
+
+    public TypeMouvement() {
+
+    }
+
+    @Override
+    public String toString() {
+        return "TypeMouvement{" +
+                "codeTypeMvt='" + codeTypeMvt + '\'' +
+                ", libelleTypeMvt='" + libelleTypeMvt + '\'' +
+                ", prixTypeMvt=" + prixTypeMvt +
+                '}';
+    }
+
+    public String getLibelleTypeMvt() {
+        return libelleTypeMvt;
+    }
+
+    public void setLibelleTypeMvt(String libelleTypeMvt) {
+        this.libelleTypeMvt = libelleTypeMvt;
+    }
+
+    public long getPrixTypeMvt() {
+        return prixTypeMvt;
+    }
+
+    public void setPrixTypeMvt(long prixTypeMvt) {
+        this.prixTypeMvt = prixTypeMvt;
+    }
+
+    public void setCodeTypeMvt(String codeTypeMvt) {
+        this.codeTypeMvt = codeTypeMvt;
+    }
+
+    public String getCodeTypeMvt() {
+        return codeTypeMvt;
+    }
+}
